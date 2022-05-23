9fd2a8c718e86c11bb0eab6b2b4db25f78826d6c
Hareram Yadav <hareramyadav@Harerams-MacBook-Pro.local>
2022-05-23 15:26:33 +0545

    some changes

--- nasa/settings.py
+++ nasa/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(#j2&xt=#^%w@3yj5=d4n%_#j#abrv5hj(+f7mp&5r5*3&ry8a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'demo.radiantnepal.info']
 
 
 # Application definition
---
+++ staticfiles/js/other.js
@@ -0,0 +1,121 @@
+$(document).ready(function () {
+    imageDiv = $('#images').show();
+    videoDiv = $('#videos').hide();
+    $('#image_button').click(function () {
+        imageDiv.show();
+        videoDiv.hide();
+    })
+    $('#video_button').click(function () {
+        imageDiv.hide();
+        videoDiv.show();
+    })
+    $('.delete_banner').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    })
+    $('.delete_footer').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    })
+    $('.delete_menu').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    })
+    $('.delete_about').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    })
+    $('.delete_news').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    })
+    $('.delete_media').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    })
+    $('.delete_blog').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    })
+    $('.delete_testimonial').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    });
+    $('.delete_popup').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    });
+    $('.delete-form').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    });
+    $('.delete_sub_menu').click(function () {
+        if (!confirm("Are you sure you want to delete?")) {
+            return false;
+        }
+    });
+
+    $('#popup').modal('show');
+
+    // API call for Menu..................................
+    // ......................................
+    // var menuIdInSubMenu = [];
+
+    $.when($.ajax("/get_menu"), $.ajax('/get_sub_menu')).done(function (menu, subMenu) {
+        var mainMenu = menu[0].data;
+        var subMenuList = subMenu[0].data;
+
+        mainMenu.map(a => {
+            if (a.menu_position === "topheader") {
+                $('.top-header').append(`<li class="nav-item"><a class="nav-link bottom-nav-link text-capitalize" href=${a.menu_link}>` + a.menu_name + '</a></li>')
+            } else {
+                if (a.menu_type === "dropdown") {
+                    var mainMenuId = a.id;
+                    console.log("menu id", mainMenuId);
+                    var filteredSubMenu = subMenuList.filter((x) => x.menu_id === mainMenuId)
+                    $('.bottom-header').append(
+                        `<div class="nav-item drop-menu dropdown" id="${a.id}">
+                            <button type="button" class="nav-link bottom-nav-link text-capitalize dropdown-toggle input-id" 
+                            id="dropdown" data-bs-toggle="dropdown"
+                            aria-expanded="false">`
+                        + a.menu_name + '</button>' +
+                        `<ul class="dropdown-menu" aria-labelledby="dropdown" id="submenu-${a.id}">` +
+                        '</ul></div>'
+                    )
+                    filteredSubMenu.map(b => {
+                        $(`#submenu-${mainMenuId}`).append(`<li><a class="dropdown-item" href="http://127.0.0.1:8000/sub_menu/${b.id}">` + b.sub_menu_name + '</a></li>')
+                    })
+                } else {
+                    $('.bottom-header').append(
+                        `<a class="nav-link text-capitalize bottom-nav-link text-dark" href="${a.menu_link}">`
+                        + a.menu_name +
+                        '</a>'
+                    )
+                }
+            }
+        })
+        console.log("new menu", mainMenu);
+        console.log("new sub menu", subMenuList);
+    });
+
+    // Input based on selected value....................
+    var linkContent = $('.link-content').hide();
+    $('#menu-type').change(function(){
+        var val = $(this).val();
+        if(val === 'link'){
+            linkContent.show();
+        }else{
+            linkContent.hide();
+        }
+    })
+});
