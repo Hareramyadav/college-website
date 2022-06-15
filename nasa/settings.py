332f4f76e5dfe4186f64d788858b6f7812864000
Hareram <nivasolution18@gmail.com>
2022-06-15 10:49:02 +0545

    static path changed

--- nasa/settings.py
+++ nasa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(#j2&xt=#^%w@3yj5=d4n%_#j#abrv5hj(+f7mp&5r5*3&ry8a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
